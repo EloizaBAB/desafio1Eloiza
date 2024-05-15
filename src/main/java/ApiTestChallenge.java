7e9f9f4994d5efdc8e10e9fe0d2556268dcdccd9
EloizaBAB <122025679+EloizaBAB@users.noreply.github.com>
2024-05-15 14:23:41 +0100

    updated project

--- src/main/java/ApiTestChallenge.java
+++ src/main/java/ApiTestChallenge.java
@@ -1,57 +1,61 @@
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.everyItem;
 import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
+
 
 import io.cucumber.core.internal.com.fasterxml.jackson.databind.jsonFormatVisitors.JsonAnyFormatVisitor;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.module.jsv.JsonSchemaValidator;
-public class TestApiChallenge {
 
-    String responseSchema = "{" +
-            "\"type\": \"array\"," +
-            "\"items\": {" +
-            "\"type\": \"object\"," +
-            "\"properties\": {" +
-            "\"id\": {\"type\": \"number\"}," +
-            "\"user_id\": {\"type\": \"number\"}," +
-            "\"title\": {\"type\": \"string\"}," +
-            "\"due_on\": {\"type\": \"string\", \"format\": \"date-time\"}," +
-            "\"status\": {\"type\": \"string\"}" +
-            "}," +
-            "\"required\": [\"id\", \"user_id\", \"title\", \"due_on\", \"status\"]" +
-            "}" +
-            "}";
+
+import java.util.List;
+
+
+public class ApiTestChallenge {
+
+
     public void automateApiTest(){
-        RestAssured.baseURI= "https://gorest.co.in/";
+        RestAssured.baseURI= "https://gorest.co.in";
+
 
         // Send a GET request to retrieve todos
 
-        Response response = given().when().get("/todos");
+
+        Response response = given().when().get("public/v2/todos");
+
 
         //Validate status code
 
+
         response.then().statusCode(200);
+
 
         //Validate body response schema/JSON
 
-        response.then().assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath(responseSchema));
 
-        //validate if all todos have status equals to completed
+        response.then().assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemaJson"));
 
-        response.then().assertThat().body("data.status", everyItem(equalTo("completed")));
 
-        //Validate that due_on is not null
-        response.then().assertThat().body("data.due_on",everyItem(notNullValue()));
+        // filter the response array to include only the fields with completed status and validate if all todos have status equals to completed
+
+
+        List<String> completedStatusList = response.jsonPath().getList("findAll { it.status == 'completed' }.status");
+        assertThat(completedStatusList, everyItem(equalTo("completed")));
+
+
+        //Validate that due_on is not null, due_on seems to be a date
+        response.then().assertThat().body("due_on",everyItem(notNullValue()));
+
 
     }
 
 
 
-
     public static void main(String[] args) {
-        TestApiChallenge tests = new TestApiChallenge();
+        ApiTestChallenge tests = new ApiTestChallenge();
         tests.automateApiTest();
     }
 }
